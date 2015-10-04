6cc87b947bff54f8546cb75964fa43a0e471fa85
WuKong <tyler.boright@gmail.com>
2015-10-04 14:28:10 +0800

    paths determine whether or not the current location is connected to others and either move the location or send back a console.log error

--- first.js
+++ first.js
@@ -1,26 +1,51 @@
-import {Location} from './location'
 import {rooms} from './room-config.js'
 
 var locationMatrix = {}
+
 for (var i = 0, n = rooms.length; i < n; i++) {
   let room = rooms[i]
   let roomKey = rooms[i].key
   locationMatrix[roomKey] = room
 }
 
-console.log(locationMatrix['dr_lights_lab'].directions)
-
 if (locationMatrix['dr_lights_lab'].directions.includes('the_fields')) {
 }
 
-function moveLocations (newLocation) {
-  console.log(newLocation)
+function startGame () {
+  let startingLocation = locationMatrix['dr_lights_lab']
   return {
-    currentRoom: newLocation.name,
-    directions: newLocation.directions
+    currentRoomKey: startingLocation.key,
+    currentRoomName: startingLocation.name,
+    currentRoomDesc: startingLocation.desc,
+    currentDirections: startingLocation.directions
   }
 }
 
-moveLocations(locationMatrix['dr_lights_lab'])
+function takePath (locationKey) {
+  if (locationKey === currentLocation.currentRoomKey) {
+    console.log('you are currently in this location')
+  }
 
-// iterate(locations)
+  if (currentLocation.currentDirections.includes(locationKey)) {
+    return moveLocation(locationMatrix[locationKey])
+  } else {
+    return console.log('that is not a path to go to.')
+  }
+}
+
+function moveLocation (newLocation) {
+  let newLoc = {
+    currentRoomKey: newLocation.key,
+    currentRoomName: newLocation.name,
+    currentRoomDesc: newLocation.desc,
+    currentDirections: newLocation.directions
+  }
+  currentLocation = newLoc
+  console.log(currentLocation)
+}
+
+let currentLocation = startGame()
+takePath('the_fields')
+takePath('the_fields')
+takePath('the_fields')
+
