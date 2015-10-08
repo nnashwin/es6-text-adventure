1c717edbfae491955f1276ba3b08a2f4e463429a
WuKong <tyler.boright@gmail.com>
2015-10-08 12:09:54 +0800

    deleted unnecessary files, added styles to the input field, wrote an input field function that grabs the value of the input and removes the previously typed text when you hit enter.  next i will add more script and begin to write the text command parser

--- src/script.js
+++ src/script.js
@@ -5,16 +5,32 @@ let AdventureGameObject = AdventureGameObject || {}
   let screen = document.getElementById('screen')
 
   /* start the game */
-  window.onload = function() {
-    addText(Game.currentLocation)
+  window.onload = function () {
+    addAreaText(Game.currentLocation)
     for (let i = 0; i < 100; i++) {
-      addText(Game.currentLocation)
+      addAreaText(Game.currentLocation)
+    }
+    getUserInput()
+  }
+
+  function getUserInput () {
+    let userInputField = document.getElementById('text-input')
+    userInputField.onkeypress = function (e) {
+      let event = e || window.event
+      let keyCode = event.keyCode || event.which
+      if (keyCode === 13) {
+        let inputText = this.value
+        this.value = ''
+        console.log(inputText)
+        return inputText
+      }
     }
   }
 
-  function addText (location) {
+  function addAreaText (location) {
     console.log(location)
     let newTitleDiv = document.createElement('div')
+    newTitleDiv.classList.add('ta-c', 'pb-5')
     let newTitleText = document.createTextNode(location.currentRoomName)
     let newDescDiv = document.createElement('div')
     let newDescText = document.createTextNode(location.currentRoomDesc)
@@ -22,6 +38,7 @@ let AdventureGameObject = AdventureGameObject || {}
     document.getElementById('text').appendChild(newTitleDiv)
     document.getElementById('text').appendChild(newBreak)
     document.getElementById('text').appendChild(newDescDiv)
+    document.getElementById('text').appendChild(newBreak)
     newTitleDiv.appendChild(newTitleText)
     newDescDiv.appendChild(newDescText)
   }
